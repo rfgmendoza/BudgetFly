402d70f4a0cba398b7db10cbb40b437134488f90
rfgmendoza <rfgmendoza@gmail.com>
2018-08-22 01:05:20 -0500

    budgetsettings now stored in sembast db

--- budget_fly/lib/fragments/budget_config.dart
+++ budget_fly/lib/fragments/budget_config.dart
@@ -1,26 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:budget_fly/pages/home_page.dart';
 import 'package:budget_fly/share/database_common.dart'
-    show DBCommon, PayPeriodType;
+    show DBCommon, PayPeriodType, BudgetSettingsModel;
 import 'package:shared_preferences/shared_preferences.dart';
-
-class BudgetSettingsModel {
-  num paycheck;
-  PayPeriodType payPeriodType;
-  int monthlyFrequency;
-  DateTime lastPayday;
-  DateTime nextPayDay;
-  List<int> calendarPayDays;
-
-  BudgetSettingsModel() {
-    paycheck = null;
-    payPeriodType = null;
-    monthlyFrequency = null;
-    lastPayday = null;
-    nextPayDay = null;
-    calendarPayDays = null;
-  }
-}
 
 class BudgetConfig extends StatefulWidget {
   @override
@@ -40,65 +22,13 @@ class BudgetConfig extends StatefulWidget {
    */
 
 class BudgetConfigState extends State<BudgetConfig> {
-  BudgetSettingsModel bsModel = new BudgetSettingsModel();
+  BudgetSettingsModel bsModel;
   TextEditingController _c;
   @override
   initState() {
-    super.initState();
     _c = new TextEditingController();
-    _initBudgetSettings();
-  }
 
-  _initBudgetSettings() async {
-    BudgetSettingsModel bsm = new BudgetSettingsModel();
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bsm.paycheck = prefs.getInt('paycheck') ?? null;
-    var type = prefs.getString('payperiodtype') ?? null;
-
-    switch (type) {
-      case "weekly":
-        bsm.payPeriodType = PayPeriodType.weekly;
-        break;
-      case "calendardate":
-        bsm.payPeriodType = PayPeriodType.calendarDate;
-        break;
-      case "monthly":
-        bsm.payPeriodType = PayPeriodType.monthly;
-        break;
-      default:
-        bsm.payPeriodType = null;
-        break;
-    }
-
-    bsm.monthlyFrequency = prefs.getInt("frequency") ?? null;
-    String lpd = prefs.getString("lastpayday") ?? null;
-    if (lpd != null) bsm.lastPayday = DateTime.parse(lpd);
-    String npd = prefs.getString("lastpayday") ?? null;
-    if (npd != null) bsm.nextPayDay = DateTime.parse(npd);
-    List<int> calendarPayDays = new List<int>();
-    List<String> calendarDates = prefs.getStringList("calendardates");
-    if (calendarDates != null)
-      calendarDates.forEach((date) => calendarPayDays.add(int.parse(date)));
-    bsm.calendarPayDays = calendarPayDays;
-    setState(() {
-      bsModel = bsm;
-    });
-  }
-
-  _saveBudgetSettings(BudgetSettingsModel bsm) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setInt("paycheck", bsm.paycheck);
-    await prefs.setString(
-        "payperiodtype", bsm.payPeriodType.toString().split('.')[1]);
-    await prefs.setInt("frequency", bsm.monthlyFrequency);
-    await prefs.setString("lastpayday", bsm.lastPayday.toString());
-    await prefs.setString("nextpayday", bsm.lastPayday.toString());
-    List<String> calendarDates = new List<String>();
-    bsm.calendarPayDays.forEach((day) => calendarDates.add(day.toString()));
-    await prefs.setStringList("calendardates", calendarDates);
-    setState(() {
-      bsm = bsm;
-    });
+    super.initState();
   }
 
   _saveInt(String key, int value) async {
@@ -109,23 +39,41 @@ class BudgetConfigState extends State<BudgetConfig> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          // here we display the title corresponding to the fragment
-          // you can instead choose to have a static title
-          title: Text("Budget Settings"),
-        ),
-        drawer: getDrawer(context),
-        body: ListView(
-          children: <Widget>[
-            //getPayCheck(context, bsModel),
-            _payCheckContent(),
-            Divider(),
-            _getPaySchedule(),
-            Divider(),
+      appBar: new AppBar(
+        // here we display the title corresponding to the fragment
+        // you can instead choose to have a static title
+        title: Text("Budget Settings"),
+      ),
+      drawer: getDrawer(context),
+      body: FutureBuilder(
+          future: DBCommon().getBudgetSettings(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return new CircularProgressIndicator();
+              default:
+                if (snapshot.hasData) {
+                  if (snapshot.data != null) {
+                    bsModel = snapshot.data as BudgetSettingsModel;
+                  } else {
+                    bsModel = new BudgetSettingsModel();
+                  }
+                  return ListView(
+                    children: <Widget>[
+                      //getPayCheck(context, bsModel),
+                      _payCheckContent(),
+                      Divider(),
+                      _getPaySchedule(),
+                      Divider(),
 
-            //payCheckCard(),
-          ],
-        ));
+                      //payCheckCard(),
+                    ],
+                  );
+                } else if (snapshot.hasError) return Text("${snapshot.error}");
+            }
+          }),
+    );
   }
 
   _payCheckContent() {
@@ -138,119 +86,117 @@ class BudgetConfigState extends State<BudgetConfig> {
         onTap: () {
           showDialog(
               context: context,
-              child: new Dialog(
-                  child: new Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: new TextField(
-                        decoration: new InputDecoration(
-                            hintText: "Amount received per paycheck"),
-                        keyboardType: TextInputType.numberWithOptions(),
-                        controller: _c,
-                      ),
-                    ),
-                    new FlatButton(
-                      child: new Text("Save"),
-                      onPressed: () {
-                        bsModel.paycheck = int.parse(_c.text);
-                        setState(() {
-                          bsModel = bsModel;
-                        });
-                        _saveInt("paycheck", bsModel.paycheck);
-                        Navigator.pop(context);
-                        //_saveBudgetSettings(bsModel);
-                      },
-                    )
-                  ])));
+              barrierDismissible: false,
+              builder: (_) => Dialog(
+                      child: new Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: new TextField(
+                            decoration: new InputDecoration(
+                                hintText: "Amount received per paycheck"),
+                            keyboardType: TextInputType.numberWithOptions(),
+                            controller: _c,
+                          ),
+                        ),
+                        new FlatButton(
+                          child: new Text("Save"),
+                          onPressed: () {
+                            bsModel.paycheck = int.parse(_c.text);
+                            DBCommon().saveBudgetSettings(bsModel);
+                            setState(() {
+                              bsModel = bsModel;
+                            });
+                            Navigator.pop(context);
+                          },
+                        )
+                      ])));
         });
   }
 
   _getPaySchedule() {
+    var _frequency = bsModel.monthlyFrequency; 
     return ListTile(
         leading: new Icon(Icons.attach_money),
         title: Text("Pay Schedule"),
         subtitle: Text(bsModel.payPeriodType != null
-            ? bsModel.paycheck.toString().split(".")[1]
+            ? bsModel.payPeriodType.toString().split(".")[1]
             : "not defined"),
         trailing: new Icon(Icons.edit),
         onTap: () {
           showDialog(
               context: context,
               barrierDismissible: false,
-              child: new Dialog(
-                                    
-                  child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      
-                      children: [
-                        
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ChoiceChip(
-                            selectedColor: Colors.green,
-                            label: //Text("option"),
+              builder: (__) => new Dialog(
+                      child: Column(mainAxisSize: MainAxisSize.min, children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ChoiceChip(
+                        selectedColor: Colors.green,
+                        label: //Text("option"),
                             Row(
-                              mainAxisSize: MainAxisSize.max,
-                              //crossAxisAlignment: CrossAxisAlignment.stretch,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text("options1"),
-                                DropdownButton(
-                                  
-                                  value: bsModel.payPeriodType == PayPeriodType.weekly ? bsModel.monthlyFrequency : null,
-                                  items: _getWeeklyDropdown(),
-                                  onChanged: (value) {
-                                    bsModel.monthlyFrequency = value;
-                                  }
-                                )
-                              ],
-                            ),
-                            selected: bsModel.payPeriodType == PayPeriodType.weekly,
-                            onSelected: (selected){ 
-                              if(selected)
-                              bsModel.payPeriodType = PayPeriodType.weekly;
-                            },
-                          ),
+                          mainAxisSize: MainAxisSize.max,
+                          //crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text("options1"),
+                            DropdownButton(
+                                value: bsModel.payPeriodType ==
+                                        PayPeriodType.weekly
+                                    ? _frequency
+                                    : null,
+                                items: _getWeeklyDropdown(),
+                                onChanged: (value) {
+                                  bsModel.monthlyFrequency = value;
+                                  DBCommon().saveBudgetSettings(bsModel);
+                                  setState(() {
+                                   _frequency = value;
+                                  });
+                                })
+                          ],
                         ),
-                        // Padding(
-                        //   padding: const EdgeInsets.all(8.0),
-                        //   child: ChoiceChip(
-                        //     label: Row(
-                        //       children: <Widget>[
-                        //         Text("options2"),
-                        //       ],
-                        //     ),
-                        //     selected: bsModel.payPeriodType == PayPeriodType.weekly,
-                        //   ),
-                        // ),
-                        // Padding(
-                        //   padding: const EdgeInsets.all(8.0),
-                        //   child: ChoiceChip(
-                        //     label: Row(
-                        //       children: <Widget>[
-                        //         Text("options3"),
-                        //       ],
-                        //     ),
-                        //     selected: bsModel.payPeriodType == PayPeriodType.weekly,
-                        //   ),
-                        // ),
-                        FlatButton.icon(
-                          label: Text("save"),
-                        icon: Icon(Icons.check),
-                        onPressed: (){
-                          
-                          setState(() {
-                            bsModel=bsModel;
-                          });
-                          Navigator.pop(context);                          
-                        },)
-                      ] 
+                        selected: bsModel.payPeriodType == PayPeriodType.weekly,
+                        onSelected: (selected) {
+                          if (selected)
+                            bsModel.payPeriodType = PayPeriodType.weekly;
+                          DBCommon().saveBudgetSettings(bsModel);
+                        },
+                      ),
+                    ),
+                    // Padding(
+                    //   padding: const EdgeInsets.all(8.0),
+                    //   child: ChoiceChip(
+                    //     label: Row(
+                    //       children: <Widget>[
+                    //         Text("options2"),
+                    //       ],
+                    //     ),
+                    //     selected: bsModel.payPeriodType == PayPeriodType.weekly,
+                    //   ),
+                    // ),
+                    // Padding(
+                    //   padding: const EdgeInsets.all(8.0),
+                    //   child: ChoiceChip(
+                    //     label: Row(
+                    //       children: <Widget>[
+                    //         Text("options3"),
+                    //       ],
+                    //     ),
+                    //     selected: bsModel.payPeriodType == PayPeriodType.weekly,
+                    //   ),
+                    // ),
+                    FlatButton.icon(
+                      label: Text("save"),
+                      icon: Icon(Icons.check),
+                      onPressed: () {
+                        setState(() {
+                          bsModel = bsModel;
+                        });
+                        Navigator.pop(context);
+                      },
                     )
-                  
-              )
-          );
+                  ])));
         });
   }
 
@@ -261,10 +207,14 @@ class BudgetConfigState extends State<BudgetConfig> {
     });
     Navigator.pop(context);
   }
-  List<DropdownMenuItem<int>> _getWeeklyDropdown(){
+
+  List<DropdownMenuItem<int>> _getWeeklyDropdown() {
     List<DropdownMenuItem<int>> list = new List<DropdownMenuItem<int>>();
-    for(int i = 1; i<=4; i++){
-      list.add(new DropdownMenuItem<int>(value: i,child: Text(i.toString()),));
+    for (int i = 1; i <= 4; i++) {
+      list.add(new DropdownMenuItem<int>(
+        value: i,
+        child: Text(i.toString()),
+      ));
     }
     return list;
   }
